0a75c830bd337ff9a72ad94ff6e2f89ce79afa7c
Jack Jiang <jsc723@berkeley.edu>
2019-02-19 04:24:02 +0000

    first commit

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,39 @@
+
+import os
+from PIL import Image
+from flask import url_for, current_app
+from flask_mail import Message
+from flaskblog import mail
+
+try:
+    from secrets import token_hex
+except ImportError:
+    from os import urandom
+    def token_hex(nbutes=None):
+        return urandom(nbytes).hex()
+
+def save_picture(form_picture):
+    random_hex = token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = '''To reset your password, visit the following link:
+{}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''.format(url_for('users.reset_token', token=token, _external=True))
+    mail.send(msg)
